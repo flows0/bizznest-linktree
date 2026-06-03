5a366e6547bb9ee81e8fd94e71f0f80428528836
bflows <billylflowers@gmail.com>
2026-06-03 01:57:00 -0700

    Refactor ToggleStatsBtn styles for improved visual consistency and interaction feedback

--- src/components/ToggleStatsBtn.tsx
+++ src/components/ToggleStatsBtn.tsx
@@ -5,9 +5,9 @@ export default function ToggleStatsBtn({ onToggle, showStats }: { onToggle: () =
     <button
       type="button"
       onClick={onToggle}
-      className={`absolute top-5 right-6 p-2 rounded-full w-fit cursor-pointer transition-colors duration-300 ease-in-out ${showStats ? "bg-brand800 ring-2 ring-brand700" : "bg-brand950"} focus:outline-none focus:border-none focus:ring-2 focus:ring-primary focus:ring-offset-brand950 focus:ring-offset-2`}
+      className={`absolute top-5 right-6 p-2 rounded-full w-fit ring-2 cursor-pointer group transition-all duration-300 ease-in-out ${showStats ? "bg-brand800 ring-primary" : "bg-brand800 ring-brand600"} focus:outline-none focus:border-none focus:ring-primary focus:ring-offset-4 focus:ring-offset-brand950`}
     >
-      <ChartLine className="size-5.25 text-brand200" />
+      <ChartLine className={`size-5.25 transition-colors duration-300 ease-in-out ${showStats ? "text-primary" : "text-brand400 group-focus:text-brand200"}`} />
     </button>
   );
 }
