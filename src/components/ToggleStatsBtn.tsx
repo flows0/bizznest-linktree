b501197006cde36bcf2d667dc4bb47130e9e6820
bflows <billylflowers@gmail.com>
2026-06-03 14:47:41 -0700

    Update ToggleStatsBtn component styles for improved hover and focus effects

--- src/components/ToggleStatsBtn.tsx
+++ src/components/ToggleStatsBtn.tsx
@@ -7,7 +7,7 @@ export default function ToggleStatsBtn({ onToggle, showStats }: { onToggle: () =
       onClick={onToggle}
       className={`absolute top-5 right-6 p-2 rounded-full w-fit ring-2 cursor-pointer group transition-all duration-300 ease-in-out ${showStats ? "bg-brand800 ring-primary" : "bg-brand800 ring-brand600"} focus:outline-none focus:border-none focus:ring-primary focus:ring-offset-4 focus:ring-offset-brand950`}
     >
-      <ChartLine className={`size-5.25 transition-colors duration-300 ease-in-out ${showStats ? "text-primary" : "text-brand400 group-focus:text-brand200"}`} />
+      <ChartLine className={`size-5.25 transition-colors duration-300 ease-in-out ${showStats ? "text-primary" : "text-brand400 group-hover:text-brand200 group-focus:text-brand200"}`} />
     </button>
   );
 }
